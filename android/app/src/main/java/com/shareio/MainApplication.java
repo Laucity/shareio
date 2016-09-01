bcb607dddf969f6fd4afc6bfa4ce04f570a130b1
Monu <prankum2100@berkeley.edu>
2016-08-31 21:38:42 -0700

    pzk - added external libs for spinkit, animatable, fb login, all the external crap needed for fb login

--- android/app/src/main/java/com/shareio/MainApplication.java
+++ android/app/src/main/java/com/shareio/MainApplication.java
@@ -3,19 +3,30 @@ package com.shareio;
 import android.app.Application;
 import android.util.Log;
 
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.FacebookSdk;
 
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.magus.fblogin.FacebookLoginPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    AppEventsLogger.activateApp(this);
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -27,8 +38,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNSpinkitPackage(),
           new ReactMaterialKitPackage(),
-          new VectorIconsPackage()
+          new VectorIconsPackage(),
+          new FacebookLoginPackage()
       );
     }
   };
